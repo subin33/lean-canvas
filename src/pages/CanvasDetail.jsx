5b85fd33018eb227dfdf24ec9b51e6030bba86ba
subin33 <shtnqls33@gmail.com>
2025-08-01 10:14:37 +0900

    feat: add CanvasTitle component with title editing functionality

--- src/pages/CanvasDetail.jsx
+++ src/pages/CanvasDetail.jsx
@@ -1,18 +1,9 @@
-import { useLocation, useParams, useSearchParams } from 'react-router-dom';
+import CanvasTitle from '../components/CanvasTitle';
 
 function CanvasDetail() {
-  const { id } = useParams();
-  const [searchParams] = useSearchParams();
-  console.log('searchParams: ', searchParams.get('keyword'));
-
-  const location = useLocation();
-  console.log('location: ', location);
   return (
     <div>
-      CanvasDetail
-      <p>id: {id}</p>
-      <p>keyword: {searchParams.get('keyword')}</p>
-      <p>hash: {location.hash}</p>
+      <CanvasTitle />
     </div>
   );
 }
